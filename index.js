608064c17b75876c25a6e2eff360e8069c0a332f
iqeiq <efutear@gmail.com>
2017-07-09 17:32:23 +0900

    remove hello

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 const WebSocket = require('ws');
 const uuid = require('node-uuid');
 
-
 // constant 
 const WEBSOCKET_PORT = process.env.PERIDOT_WEBSOCKET_PORT || process.env.PORT || 8080;
 
@@ -20,7 +19,6 @@ wss.broadcast = (data, ws) => {
 wss.on('connection', (ws, req) => {
     ws.uuid = uuid.v4();
     console.log('connect:', ws.uuid, '/ ip =', req.connection.remoteAddress);
-    ws.send(JSON.stringify({ type: 'hello', uuid: ws.uuid }));
     ws.on('message', (data) => {
         console.log('message:', data)
         wss.broadcast(data, ws)
